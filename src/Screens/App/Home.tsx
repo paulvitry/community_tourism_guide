9bf426f3fd1848b506857f43ea8579f2c2d2f2b9
Paul Vitry <paul.vitry@weeakt.com>
2021-10-11 09:25:47 +0200

    Map + marker

--- src/Screens/App/Home.tsx
+++ src/Screens/App/Home.tsx
@@ -1,33 +1,56 @@
-import React, { useContext } from 'react';
-import { StyleSheet, Text, Button } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { StyleSheet, View, Text, Button, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import MapView, { Marker } from 'react-native-maps';
 import { NavigationProps } from '../../Navigation/Navigation';
 
 import { AuthenticationContext } from '../../Contexts/AuthenticationContext';
-
 
 type IHomeProps = NavigationProps<'Home'>;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    //  alignItems: 'center',
+    // justifyContent: 'center',
   },
 
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
 
-export const Home: React.FC<IHomeProps> = ({  }) => {
+export const Home: React.FC<IHomeProps> = ({}) => {
   const { logout } = useContext(AuthenticationContext);
+  const [markers, setMarkers] = useState([
+    {
+      latlng: { latitude: 48.864716, longitude: 2.349014 },
+      title: 'paris',
+      description: 'description',
+    },
+  ]);
 
   const onClick = async () => {
     console.log('logout');
     await logout();
   };
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Home</Text>
-      <Button title="logout" onPress={onClick}/>
-      
-    </SafeAreaView>
+    <View style={styles.container}>
+      <MapView style={styles.map}>
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
+      <SafeAreaView style={styles.container}>
+        <Text>Home</Text>
+        <Button title="logout" onPress={onClick} />
+      </SafeAreaView>
+    </View>
   );
 };
